f3576cf1353a0b85f11cc39e7aa62329e0822f29
KJ Kim <gencode.me@gmail.com>
2021-02-16 23:54:21 +0900

    set publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  outputDir: 'docs'
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tiptap-component/'
+    : '/'
 }
